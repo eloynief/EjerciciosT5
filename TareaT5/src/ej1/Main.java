55cbe010372ab6c2b58cd56930d1400eb119b3bc
enieves <enieves@106-02.info2>
2023-02-09 14:35:49 +0100

    Terminar los pasos hechos en papel y terminar el main

---
+++ TareaT5/src/ej1/Main.java
@@ -0,0 +1,11 @@
+package ej1;
+
+public class Main {
+//para meter valores
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num;
+		int[] busca = Buscaminas.main(num);
+	}
+
+}
